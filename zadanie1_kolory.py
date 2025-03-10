83dcba3e4e0c5a2bbce8933bc95abf0d5d15e5ec
Agnieszka Nosek <97952@student.ue.poznan.pl>
2025-03-10 10:21:22 +0100

    Adding file with exercise 1 code

---
+++ zadanie1_kolory.py
@@ -0,0 +1,8 @@
+red_candy = 10
+green_candy = 5
+yellow_candy = 15
+purple_candy = 3
+how_many_candies = red_candy + green_candy + yellow_candy + purple_candy
+def prawdopodobienstwo1(purple_candy, how_many_candies):
+    print(purple_candy/how_many_candies)
+prawdopodobienstwo1(purple_candy, how_many_candies)
